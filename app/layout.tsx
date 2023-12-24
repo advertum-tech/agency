076ea63a7238e06d71da42e85ed6699a1a0b85a5
shabak <nikolay.shabak@gmail.com>
2023-12-25 01:22:47 +0200

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type {Metadata} from 'next'
 import {Inter, Roboto_Mono} from 'next/font/google'
 import cn from 'classnames';
 import './assets/styles/globals.css'
+import oona from './assets/styles/oona/oona.module.scss'
 import './assets/styles/materialize.css'
 import './assets/styles/linea.min.css'
-import oona from './assets/styles/oona/oona.module.scss'
 import Nav from "@/app/layout/nav";
 import Footer from "@/app/layout/footer";
 
